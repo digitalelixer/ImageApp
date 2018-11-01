936fc65f6ac2dd7d801da52f870ad59613ce61b6
Alex Lemp <alemp@uncc.edu>
2018-11-01 14:55:18 +0000

    Added create and show functionality

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,20 @@
 class ImagesController < ApplicationController
+    def show
+        @image = Image.find(params[:id])
+    end
+    
     def new
     end
     
     def create
+        @image = Image.new(image_params)
+        
+        @image.save
+        redirect_to @image
     end
 end
+
+private
+    def image_params
+        params.require(:images).permit(:title, :text)
+    end
